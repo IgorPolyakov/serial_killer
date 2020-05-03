bd586cddf9edc7024e78956df8cbdbdc57b24ad4
Ev Bogdanov <evbogdanov.com@gmail.com>
2020-05-03 21:05:29 +0300

    Refactor DB queries

--- kekend/lib/serial_killer/hinter.ex
+++ kekend/lib/serial_killer/hinter.ex
@@ -1,22 +1,11 @@
 defmodule SerialKiller.Hinter do
+  alias SerialKiller.DB
+
   def get_hints(title) do
-    Postgrex.query!(
-      SerialKiller.DB,
-      "SELECT title, id, start_year FROM shows WHERE title ILIKE '%' || $1 || '%' LIMIT 10",
+    DB.query!(
+      "SELECT title, id, start_year FROM shows
+       WHERE title ILIKE '%' || $1 || '%' LIMIT 10",
       [title]
     )
-    |> result_to_maps
-  end
-
-  def result_to_maps(%Postgrex.Result{columns: _, rows: nil}), do: []
-
-  # FIXME: move it to helpers?
-  def result_to_maps(%Postgrex.Result{columns: col_nms, rows: rows}) do
-    Enum.map(rows, fn row -> row_to_map(col_nms, row) end)
-  end
-
-  defp row_to_map(col_nms, vals) do
-    Stream.zip(col_nms, vals)
-    |> Enum.into(Map.new(), & &1)
   end
 end
