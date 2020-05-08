28dcbf1d7205d66cf54feab8022be52f8c609f5e
Ev Bogdanov <evbogdanov.com@gmail.com>
2020-05-08 23:54:04 +0300

    Select best shows

--- kekend/lib/serial_killer/best.ex
+++ kekend/lib/serial_killer/best.ex
@@ -1,5 +1,20 @@
 defmodule SerialKiller.Best do
+  alias SerialKiller.DB
+
+  # TODO: maybe make it configurable via query params
+  @limit 100
+
+  # TODO: think how to calculte best shows
+  @min_num_votes_to_consider_show_good 100_000
+
   def get_best_shows do
-    [%{best_shows: "OK"}]
+    DB.query!(
+      "SELECT *
+       FROM shows
+       WHERE num_votes > #{@min_num_votes_to_consider_show_good}
+       ORDER BY rating DESC
+       LIMIT #{@limit}",
+      []
+    )
   end
 end
