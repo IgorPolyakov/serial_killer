4576daef72414c7ca844bcb9d8cf7d02d181c6b0
Ev Bogdanov <evbogdanov.com@gmail.com>
2020-05-09 02:55:31 +0300

    Make TypeScript work with React

---
+++ frontend/src/components/Hello/index.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+interface Props {
+  to: string
+}
+
+const Hello: React.FC<Props> = ({ to }) => (
+  <h1>Hello, {to}!</h1>
+)
+
+export default Hello
--- frontend/src/index.ts
+++
@@ -1,5 +0,0 @@
-let message: string
-
-message = 'Hello, Serial Killer'
-
-console.log(message)
