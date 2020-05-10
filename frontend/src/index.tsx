4ea8ee547272300d960131609cfb08248b58dd3b
Ev Bogdanov <evbogdanov.com@gmail.com>
2020-05-10 22:34:06 +0300

    Test mobx

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,9 +1,14 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
+import 'mobx-react-lite/batchingForReactDom'
 
 import Hello from './components/Hello'
+import TestMobx from './components/TestMobx'
 
 ReactDOM.render(
-  <Hello to="Serial Killer" />,
+  <div>
+    <Hello to="Serial Killer" />
+    <TestMobx />
+  </div>,
   document.getElementById('serial-killer')
 )
