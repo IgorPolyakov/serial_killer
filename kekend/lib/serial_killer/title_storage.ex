2763a2ac056b50270346a5bf43c54c621ed5d5e3
IgorPolyakov <igorpolyakov@protonmail.com>
2020-05-10 12:14:09 +0300

    init v2

--- kekend/lib/serial_killer/title_storage.ex
+++ kekend/lib/serial_killer/title_storage.ex
@@ -1,12 +1,28 @@
 defmodule SerialKiller.TitleStorage do
   def init do
-    :ets.new(:title_storage, [:set, :protected, :named_table])
+    :ets.new(:title_storage, [:bag, :protected, :named_table])
 
-    Postgrex.query!(
-      SerialKiller.DB,
-      "SELECT title, id FROM shows",
-      []
-    ).rows
-    |> Enum.each(fn x -> :ets.insert(:title_storage, {Enum.at(x, 0), Enum.at(x, 1)}) end)
+    shows =
+      Postgrex.query!(
+        SerialKiller.DB,
+        "SELECT title, num_votes, id FROM shows ORDER BY num_votes desc LIMIT 10000",
+        []
+      ).rows
+
+    build_one_letter_index(shows)
+  end
+
+  defp build_one_letter_index(shows) do
+    Enum.each(shows, fn row ->
+      String.split(Enum.at(row, 0), " ")
+      |> Enum.each(fn word ->
+        letters =
+          word
+          |> String.at(0)
+          |> String.downcase()
+
+        :ets.insert(:title_storage, {letters, row})
+      end)
+    end)
   end
 end
