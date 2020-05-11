29a59404e8cac5f01652cae5aa8b3e5caa2872d1
Ev Bogdanov <evbogdanov.com@gmail.com>
2020-05-11 18:51:03 +0300

    We don't need MobX right now

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
-import 'mobx-react-lite/batchingForReactDom'
 import App from '~/components/App'
 
 ReactDOM.render(<App />, document.getElementById('serial-killer'))
