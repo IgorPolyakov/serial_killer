8ce9a601ee5404134c1f78e7ebf6c15ac852f0e9
Ev Bogdanov <evbogdanov.com@gmail.com>
2020-05-13 03:37:36 +0300

    Import type

--- frontend/src/pages/Search/components/Shows/index.tsx
+++ frontend/src/pages/Search/components/Shows/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import { Link } from 'react-router-dom'
-import { TShow, TShows } from '~/types'
+import type { TShow, TShows } from '~/types'
 
 interface TProps {
   shows: TShows
