07fc7804ea1bc4c4a2001c5ab6f905b07bdbdace
IgorPolyakov <igorpolyakov@protonmail.com>
2020-05-22 23:58:22 +0300

    init v4

--- kekend/lib/serial_killer/title_storage.ex
+++ kekend/lib/serial_killer/title_storage.ex
@@ -11,11 +11,11 @@ defmodule SerialKiller.TitleStorage do
     shows =
       Postgrex.query!(
         SerialKiller.DB,
-        "SELECT title, num_votes, id FROM shows ORDER BY num_votes desc LIMIT 10000",
+        "SELECT title, num_votes, id FROM shows ORDER BY num_votes desc LIMIT 100",
         []
       ).rows
 
-    build_one_letter_index(shows)
+    build_letters_index(shows)
     :ok
   end
 
@@ -29,17 +29,50 @@ defmodule SerialKiller.TitleStorage do
     end)
   end
 
-  defp build_one_letter_index(shows) do
+  @doc """
+  shows -> [["Game of Thrones", 1668725, "tt0944947"]]
+  """
+  defp build_letters_index(shows) do
     Enum.each(shows, fn row ->
-      String.split(Enum.at(row, 0), " ")
-      |> Enum.each(fn word ->
-        letters =
-          word
-          |> String.at(0)
-          |> String.downcase()
+      show = Enum.at(row, 0)
 
-        Ets.insert(:title_storage, {letters, row})
+      String.split(show, " ")
+      |> Enum.each(fn word ->
+        first_letter = function_name(word, 0)
+        set_to_title_storage(first_letter, row)
+
+        second_letter = function_name(word, 1)
+        set_to_title_storage("#{first_letter}#{second_letter}", row)
+
+        third_letter = function_name(word, 2)
+        set_to_title_storage("#{first_letter}#{second_letter}#{third_letter}", row)
+
+        four_letter = function_name(word, 3)
+        set_to_title_storage("#{first_letter}#{second_letter}#{third_letter}#{four_letter}", row)
       end)
     end)
   end
+
+  defp set_to_title_storage(index, show) do
+    Ets.insert(
+      :title_storage,
+      {index, show}
+    )
+  end
+
+  defp function_name(word, deep) do
+    word
+    |> String.at(deep)
+    |> prepare_letter
+  end
+
+  defp prepare_letter(letter) do
+    cond do
+      letter == nil -> ""
+      String.match?(letter, ~r/\w/) -> String.downcase(letter)
+      true -> ""
+    end
+  end
 end
+
+# SerialKiller.TitleStorage.init(:ok)
